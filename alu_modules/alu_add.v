288b0d727274e280b769a8bebfe843120f1f1997
CeicGitHub <cesar.inda@iteso.mx>
2025-10-12 17:24:28 -0700

    Macros_ALU_Operations_RTL Added

---
+++ alu_modules/alu_add.v
@@ -0,0 +1,11 @@
+module alu_add (
+`ifdef USE_POWER_PINS
+    inout vccd1,
+    inout vssd1,
+`endif
+    input  [7:0] A,
+    input  [7:0] B,
+    output [7:0] Y
+);
+    assign Y = A + B;
+endmodule
